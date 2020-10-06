650b675086b17210bd5f342aa695f72c85bf83af
Ackyla Ferreira Batista <ackylafb@gmail.com>
2020-10-06 12:43:23 -0300

    added https

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,14 @@ const routes = express.Router();
 
 const ContentController = require('./App/controlers/contentController');
 
+
+routes.get('*', (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] != 'https') {
+    res.redirect("https://" + req.headers.host + req.url);
+  } else {
+    next();
+  }
+});
 routes.get("/", ContentController.index);
 routes.get("/discipulado/:id", ContentController.show);
 
